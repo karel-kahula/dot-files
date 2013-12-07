12cf4ee2bf08b7be4f13d016cc69addc36f5a483
Karel Kahula <karel.kahula@telenium.ca>
2013-12-06 19:33:05 -0600

    added submodule setup to shell script

--- setup.sh
+++ setup.sh
@@ -2,9 +2,13 @@
 files="bashrc gitconfig gitignore vimrc vim"
 dotfilesdir=~/dev/dot-files
 
+# pull all latest bundles
+git submodule update --init
+
+# copy dot files and directories
 cd ~
 for file in $files; do
-  rm ~/.$file
+  rm  -f ~/.$file
   ln -s $dotfilesdir/$file ~/.$file
 done
 

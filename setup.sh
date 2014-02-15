7169fc11913726b33e227cd68c6bbff29a673fc0
Karel Kahula <karel.kahula@telenium.ca>
2014-02-15 13:16:03 -0600

    adding powerline symbols setup and some git something.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 files="bashrc gitconfig gitignore vimrc vim git-prompt.sh"
 dotfilesdir=$(pwd)
 
@@ -11,4 +12,11 @@ for file in $files; do
   rm  -rf ~/.$file
   ln -s $dotfilesdir/$file ~/.$file
 done
+
+# setup powerline symbols.
+mkdir -p ~/.fonts
+mkdir -p ~/.fonts.conf.d
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf ~/.fonts/PowerlineSymbols.otf
+fc-cache -vf ~/.fonts
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf ~/.fonts.conf.d/10-powerline-symbols.conf
 
